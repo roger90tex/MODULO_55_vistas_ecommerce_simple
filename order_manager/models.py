d289870c12ddd5577c74d96d3302cc0735d3e217
Rogelio Lopez Ramirez <roger90tex@gmail.com>
2026-04-29 12:27:36 -0600

    Registrando modelos en el Django Admin

--- order_manager/models.py
+++ order_manager/models.py
@@ -4,8 +4,9 @@ from cart.models import Cart
 
 class Order(models.Model):
     cart = models.OneToOneField(Cart, on_delete=models.CASCADE)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    status = models.CharField(max_length=50, default='Pendiente')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Orden #{self.id}"
+        return f"Orden #{self.id} - {self.status}"
